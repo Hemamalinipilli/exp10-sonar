6867e1383d8dcfc7dece422b67dd7649d03146ef
honey <honey@LAPTOP-R6451UU6>
2022-11-29 15:18:23 +0530

    first

---
+++ src/main/java/EXP10_1/EXP10_1/App.java
@@ -0,0 +1,13 @@
+package EXP10_1.EXP10_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/EXP10_1/EXP10_1/AppTest.java
@@ -0,0 +1,20 @@
+package EXP10_1.EXP10_1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
